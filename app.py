46868bce23ad7c5e7968f979ebb2cc23ddadce57
nabin <nkbhaie2017@gmail.com>
2021-11-30 20:59:01 -0700

    add app.py file

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+# from flask.templating import render_template
+from forms import AddSnacksForm   # from module import class
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "whatthehell"  # secret key is needed to use CSRF in the forms in wtforms, for flash messaging in flask
 
 # To check if flask is up and running
 @app.route('/')
@@ -10,3 +14,10 @@ def home_route():
 @app.route('/name')
 def get_name():
     return "<h5> Nabin &&&&&&& Niroula </h5>"
+
+@app.route("/snacks/new")
+def add_snack():
+    form = AddSnacksForm()   # form is a form object
+    #raise # raise is used to get error so that you can get interactive debugger tool in browser
+    return render_template("add_snack_form.html", form = form)
+
